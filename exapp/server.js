6f46fe35459c3fe359c92d76f9e71937d1e0120a
jojahn <jj.media@web.de>
2017-12-21 16:14:24 +0100

    database-functions completed

--- exapp/server.js
+++ exapp/server.js
@@ -76,7 +76,7 @@ app.get('/db/all', (req, res) => {
 
 /**
 /* function: get all surveys by creator
-/* GET: server/db/<your_name>
+/* GET: server/db/all/<your_name>
 /* Response: [{ creator="<your_name>", "title":"<actual_question>",
 /* "type":"<type>","state":"<state>", "date":"<creation_date>",
 /* "invite":"<question_key>", "correct":"<correct_answer>","answer_A":"<Answer_A>", 
@@ -84,7 +84,7 @@ app.get('/db/all', (req, res) => {
 /* "answer_D":"<Answer_D>"}, ...]
 /* Expected Code: 200 Ok
 */
-app.get('/db/:name', (req, res) => {
+app.get('/db/all/:name', (req, res) => {
 	con.then(() => {
 		return db.query("SELECT * FROM surveys WHERE creator=? ORDER BY state DESC, date", [req.params.name]);
 	}).then((result) => {
@@ -98,7 +98,7 @@ app.get('/db/:name', (req, res) => {
 });
 
 /**
-/* function: get one survey by invite
+/* function: get one survey by invite-key
 /* GET: server/db/<key>
 /* Response: [{ creator="<your_name>", "title":"<actual_question>",
 /* "type":"<type>","state":"<state>", "date":"<creation_date>",
@@ -128,7 +128,8 @@ app.get('/db/:key', (req, res) => {
 /**
 /*	function: start one survey
 /*	POST: server/db/start?name=<your_name>&key=<invite>
-/*	Response: no body, 200 Ok
+/*	Response: no body
+/* 	Expected Code: 200 Ok
 */
 app.post('/db/start', (req, res) => {
 	con.then(() => {
@@ -141,19 +142,18 @@ app.post('/db/start', (req, res) => {
 		console.log(err);
 		res.status(400).send();
 		throw (err);
-	});  
+	});
 })
 
 /**
 /*	function: stop all surveys for one creator
 /*	POST: server/db/questions/stop?name=<your_professor_name>
-/*	Response: no body, 200 Ok
+/*	Response: no body
+/* 	Expected Code: 200 Ok
 */
 app.post('/db/stop', (req, res) => {
 	con.then(() => {
-		return db.query("UPDATE lectures SET state = 0 WHERE creator=?;", [req.query.name]);
-	}).then(() => {
-		return db.query("UPDATE questions SET state = 0 WHERE professor=?;", [req.query.name]);
+		return db.query("UPDATE surveys SET state = 0 WHERE creator=?;", [req.query.name]);
 	}).then(() => {
 		res.status(200).send();
 	}).catch((err) => {
@@ -165,16 +165,19 @@ app.post('/db/stop', (req, res) => {
 
 /**
 /*	function: add new survey
-/*	PUT: server/db/questions/new with body "name=<professor_name>&id=<lecture_id>&course=<course_name>&title=<title>&type=<type>&correct=<correct_answer>&a=<Answer_A>&b=<Answer_B>&c=<Answer_C>&d=<Answer_D>"
-/*	Response: no body, 200 Ok
+/*	PUT: server/db/questions/new with body "name=<your_name>&
+/*	title=<title>&type=<type>&correct=<correct_answer>&
+/*	a=<Answer_A>&b=<Answer_B>&c=<Answer_C>&d=<Answer_D>"
+/*	Response: no body
+/* 	Expected Code: 200 Ok
 */
 app.put('/db/new', (req, res) => {
-	var id = req.body.id.toLowerCase(); var name = req.body.name.toLowerCase(); var course = req.body.course.toLowerCase(); 
-	var semester = tools.calcSemester(); var title = req.body.title;
-	var invite = tools.generateKey(); var date = new Date(); var type = req.body.type; var correct = req.body.correct;
+	var creator = req.body.name;
+	var title = req.body.title; var invite = tools.generateKey(); var date = new Date(); 
+	var type = req.body.type; var correct = req.body.correct;
 	var A = req.body.a;	var B = req.body.b;	var C = req.body.c;	var D = req.body.d;
 	con.then(() => {
-		return db.query("INSERT INTO `questions` (`id`, `professor`, `course`, `semester`, `invite`, `title`, `date`, `type`, `correct`, `answer_A`, `answer_B`, `answer_C`, `answer_D`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);", [id,name,course,semester,invite,title,date,type,correct,A,B,C,D]);
+		return db.query("INSERT INTO `surveys` (`creator`, `invite`, `title`, `date`, `type`, `correct`, `answer_A`, `answer_B`, `answer_C`, `answer_D`) VALUES (?,?,?,?,?,?,?,?,?,?);", [creator,invite,title,date,type,correct,A,B,C,D]);
 	}).then((result) => {
 		res.status(201).send();
 	}).catch((err) => {
